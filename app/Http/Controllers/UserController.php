358d00f05237db247decfd9550f9bcae8e72c7a1
Matsui Guenta <matsuiguenta@gmail.com>
2022-06-24 14:50:58 -0300

    Upload Files Laravel

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Requests\StoreUpdateUserFormRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 use function PHPUnit\Framework\returnSelf;
 
@@ -51,6 +52,14 @@ class UserController extends Controller
         $data = $request->all();
         $data['password'] = bcrypt($request->password);
 
+        if ($request->image) {
+            $data['image'] = $request->image->store('users');
+
+            //$extension = $request->image->getClientOriginalExtension();
+            //$data['image'] = $request->image->storeAs('users', now() . ".{$extension}");
+
+        }
+
         $user = $this->model->create($data);
 
         return redirect()->route('users.index');
@@ -79,6 +88,16 @@ class UserController extends Controller
         $data = $request->only('name', 'email');
         if ($request->password)
             $data['password'] = bcrypt($request->password);
+
+        if ($request->image) {
+            if ($user->image && Storage::exists($user->image)) {
+                Storage::delete($user->image);
+            }    
+
+            $data['image'] = $request->image->store('users');
+        }
+            //$extension = $request->image->getClientOriginalExtension();
+            //$data['image'] = $request->image->storeAs('users', now() . ".{$extension}");    
 
         $user->update($data);   
 
